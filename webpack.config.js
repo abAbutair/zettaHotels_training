471b781c08cc02026f1a75dbfabce77329ff05aa
Abdulrahman Abutair <ab.abutair@gmail.com>
2021-05-05 15:18:15 +0200

    first commit with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { NetlifyPlugin } = require("netlify-webpack-plugin");
 
 module.exports = {
   entry: "./src/js/index.js",
@@ -6,4 +7,5 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     filename: "js/bundle.js",
   },
+  plugins: [new NetlifyPlugin({})],
 };
