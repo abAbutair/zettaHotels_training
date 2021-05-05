8411c26b150465ddb8c640815b7c18ba8fb7c0e8
Abdulrahman Abutair <ab.abutair@gmail.com>
2021-05-05 18:36:00 +0200

    first commit with webpack 06

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,35 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     filename: "js/bundle.js",
   },
-  plugins: [new NetlifyPlugin({})],
+  plugins: [
+    new NetlifyPlugin({
+      redirects: [
+        {
+          from: "/old-path",
+          to: "/new-path",
+          status: 301,
+          force: false,
+          query: {
+            path: ":path",
+          },
+          conditions: {
+            language: ["en", "es"],
+            country: ["US"],
+          },
+        },
+        {
+          from: "/api/*",
+          to:
+            "https://us-central1-netlify-intercom.cloudfunctions.net/readHeaders/:splat",
+          status: 200,
+          force: true,
+          conditions: {
+            role: ["admin", "cms"],
+          },
+        },
+      ],
+    }),
+  ],
   // module: {
   //   rules: [
   //     {
